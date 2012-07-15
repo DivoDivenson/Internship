8d09af687233780465c1534f3f5d5169cbd24f79
Mr. Divenson <divoapple@gmail.com>
2012-07-15 18:08:24 +0100

    Add mind map

---
+++ Technical_Report.mm
@@ -0,0 +1,97 @@
+<map version="0.9.0">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1342370977495" ID="ID_503289433" MODIFIED="1342371449673" TEXT="Technial Report">
+<node CREATED="1342371473988" ID="ID_1607575527" MODIFIED="1342371652471" POSITION="right" TEXT="Introduction">
+<icon BUILTIN="full-1"/>
+<node CREATED="1342371495733" ID="ID_839728055" MODIFIED="1342371508060" TEXT="Motivation + Description"/>
+<node CREATED="1342371510409" ID="ID_29180710" MODIFIED="1342371518109" TEXT="Goals + Objectives"/>
+<node CREATED="1342371521776" ID="ID_1363164774" MODIFIED="1342371530585" TEXT="Approach / State of the art">
+<node CREATED="1342371533313" ID="ID_579505031" MODIFIED="1342371581119">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Slightly reflective.
+    </p>
+    <p>
+      Goals with mentor.
+    </p>
+    <p>
+      Spent time learning system.
+    </p>
+    <p>
+      Learnt new skills.
+    </p>
+    <p>
+      Development
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+</node>
+<node CREATED="1342371587978" ID="ID_672305394" MODIFIED="1342371592758" TEXT="Overview of contents"/>
+</node>
+<node CREATED="1342371655155" ID="ID_668206131" MODIFIED="1342371670540" POSITION="left" TEXT="Internship in context">
+<icon BUILTIN="full-2"/>
+<node CREATED="1342371676367" ID="ID_558722749" MODIFIED="1342371681447" TEXT="SDL goes here">
+<node CREATED="1342371774330" ID="ID_1617041107" MODIFIED="1342371795761" TEXT="QA, do a good bit on that"/>
+</node>
+<node CREATED="1342371684244" ID="ID_1566301415" MODIFIED="1342371759049">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Derive requirments of testing from literature.
+    </p>
+    <p>
+      Blackbox functionality testing.
+    </p>
+    <p>
+      Derive the rest of the reuirements from actual comapny requirement
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+<node CREATED="1342371763352" ID="ID_1290825032" MODIFIED="1342371772354" TEXT="Describe exisiting system"/>
+</node>
+<node CREATED="1342371801725" ID="ID_1879027955" MODIFIED="1342371817721" POSITION="right" TEXT="Exisiting system">
+<icon BUILTIN="full-3"/>
+<node CREATED="1342371819391" ID="ID_1322342813" MODIFIED="1342371829344" TEXT="Detail in appendix"/>
+<node CREATED="1342371830823" ID="ID_59649278" MODIFIED="1342371834423" TEXT="Objectives"/>
+<node CREATED="1342371835523" ID="ID_1596358051" MODIFIED="1342371844534" TEXT="Functions in broad scope"/>
+<node CREATED="1342371846601" ID="ID_151444286" MODIFIED="1342371853845" TEXT="Architecture"/>
+<node CREATED="1342371854777" ID="ID_520717321" MODIFIED="1342371861262" TEXT="Technologies used"/>
+<node CREATED="1342371862152" ID="ID_1986518488" MODIFIED="1342371868744" TEXT="Inputes / Outputs"/>
+</node>
+<node CREATED="1342371872900" ID="ID_388078745" MODIFIED="1342371929049" POSITION="left" TEXT="SMART">
+<icon BUILTIN="full-4"/>
+<node CREATED="1342371878815" ID="ID_1066911691" MODIFIED="1342371882147" TEXT="Objectives"/>
+<node CREATED="1342371886440" ID="ID_548050359" MODIFIED="1342371891590" TEXT="Describe smart"/>
+<node CREATED="1342371892396" ID="ID_1857923824" MODIFIED="1342371902500" TEXT="Describe what was done BUGS"/>
+<node CREATED="1342371903895" ID="ID_1388699857" MODIFIED="1342371914514" TEXT="How were bugs tested / found"/>
+<node CREATED="1342371915405" ID="ID_522252457" MODIFIED="1342371918480" TEXT="Pulse / Cronos"/>
+<node CREATED="1342371919824" ID="ID_333451379" MODIFIED="1342371921168" TEXT="OIL"/>
+</node>
+<node CREATED="1342371935284" ID="ID_295041916" MODIFIED="1342371950510" POSITION="right" TEXT="Web Admin Console">
+<icon BUILTIN="full-5"/>
+<node CREATED="1342371878815" ID="ID_392964832" MODIFIED="1342371882147" TEXT="Objectives"/>
+<node CREATED="1342371886440" ID="ID_1190298495" MODIFIED="1342371972942" TEXT="Describe WebAdmin Console"/>
+<node CREATED="1342371892396" ID="ID_600876885" MODIFIED="1342371902500" TEXT="Describe what was done BUGS"/>
+<node CREATED="1342371903895" ID="ID_1911147553" MODIFIED="1342371914514" TEXT="How were bugs tested / found"/>
+<node CREATED="1342371915405" ID="ID_999140369" MODIFIED="1342371918480" TEXT="Pulse / Cronos"/>
+<node CREATED="1342371919824" ID="ID_594435941" MODIFIED="1342371921168" TEXT="OIL"/>
+</node>
+<node CREATED="1342371981765" ID="ID_1863790743" MODIFIED="1342371991144" POSITION="left" TEXT="Conclusion">
+<icon BUILTIN="full-6"/>
+<node CREATED="1342371997277" ID="ID_1593725281" MODIFIED="1342372002337" TEXT="Technical conclusions"/>
+<node CREATED="1342372003437" ID="ID_1888569730" MODIFIED="1342372006635" TEXT="Goals"/>
+<node CREATED="1342372007315" ID="ID_14931551" MODIFIED="1342372011052" TEXT="BCS Goals"/>
+</node>
+</node>
+</map>
